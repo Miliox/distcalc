43e723fbf1e709f9cf40b6d01b9457e75c7c6158
Emiliano Firmino <elmiliox@gmail.com>
2013-04-24 18:34:17 -0400

    add InfixToPostfix class and unit tests
    Signed-off-by: Emiliano Firmino <elmiliox@gmail.com>

---
+++ lib/distcalc/calculator/infix_to_postfix.rb
@@ -0,0 +1,76 @@
+require_relative './scanner'
+
+module Calculator
+  class InfixToPostfix
+    OP_POP_PRIORY = {add: 1, sub: 1, mult: 2, div: 2, open_bracket: 0}
+    OP_PUSH_PRIORY = {add: 1, sub: 1, mult: 2, div: 2, open_bracket: 3}
+
+    # Convert a infix stream of tokens to postfix one
+    #
+    # @param tokens [Array<Number, Symbol>] a stream in infix
+    # @return tokens [Array<Number, Symbol>] a stream in postfix
+    def to_postfix(input_tokens)
+      @output_tokens = Array.new
+      @operators = Array.new
+
+      input_tokens.each do |token|
+        eval_token(token)
+      end
+      pop_until_empty
+
+      @output_tokens
+    end
+
+    private
+    def eval_token(token)
+      if Scanner::OPERATORS_TOKENS.include? token
+        eval_operator(token)
+      elsif token.is_a? Numeric
+        @output_tokens.push(token)
+      else
+        raise StandardError.new "invalid token #{token}"
+      end
+    end
+
+    private
+    def eval_operator(token)
+      if token != :close_bracket
+        push_operator(token)
+      else
+        pop_until_open_bracket
+      end
+    end
+
+    private
+    def push_operator(op)
+      unless @operators.empty?
+        if OP_POP_PRIORY[@operators.last] >= OP_PUSH_PRIORY[op]
+          @output_tokens.push(@operators.pop)
+        end
+      end
+      @operators.push(op)
+    end
+
+    private
+    def pop_until_open_bracket
+      until @operators.last == :open_bracket
+        if @operators.empty?
+          raise StandardError.new 'invalid expression: not found open bracket'
+        end
+        @output_tokens.push(@operators.pop)
+      end
+      @operators.pop
+    end
+
+    private
+    def pop_until_empty
+      until @operators.empty?
+        if @operators.last != :open_bracket
+          @output_tokens.push(@operators.pop)
+        else
+          raise StandardError.new 'invalid expression: unclosed bracket'
+        end
+      end
+    end
+  end
+end
---
+++ test/distcalc/calculator/infix_to_postfix_test.rb
@@ -0,0 +1,50 @@
+require 'distcalc/calculator/infix_to_postfix'
+require 'test/unit'
+
+class TestInfixToPostfix < Test::Unit::TestCase
+  def setup
+    @conv = Calculator::InfixToPostfix.new
+  end
+
+  def test__to_postfix__example1__return_postfix_expression
+    expr = [3, :add, 4, :mult, 5, :div, 6]
+    expect = [3, 4, 5, :mult, 6, :div, :add]
+
+    assert_equal(expect, @conv.to_postfix(expr))
+  end
+
+  def test__to_postfix__example2__return_postfix_expression
+    expr = [:open_bracket, 300, :add, 23, :close_bracket,
+            :mult, :open_bracket, 43, :sub, 21, :close_bracket,
+            :div, :open_bracket, 84, :add, 7, :close_bracket]
+    expect = [300, 23, :add, 43, 21, :sub, :mult, 84, 7, :add, :div]
+
+    assert_equal(expect, @conv.to_postfix(expr))
+  end
+
+  def test__to_postfix__example3__return_postfix_expression
+    expr = [:open_bracket, 4, :add, 8, :close_bracket,
+            :mult,
+            :open_bracket, 6, :sub, 5, :close_bracket,
+            :div, :open_bracket,
+            :open_bracket, 3, :sub, 2, :close_bracket,
+            :mult, :open_bracket, 2, :add, 2, :close_bracket,
+            :close_bracket]
+    expect = [4, 8, :add, 6, 5, :sub, :mult, 3, 2,
+              :sub, 2, 2, :add, :mult, :div]
+
+    assert_equal(expect, @conv.to_postfix(expr))
+  end
+
+  def test__to_postfix__common_errors__raise_standard_error
+    assert_raise(StandardError) do
+      @conv.to_postfix([:open_bracket, 9, :add, 4])
+    end
+    assert_raise(StandardError) do
+      @conv.to_postfix([9, :add, 4, :close_bracket])
+    end
+    assert_raise(StandardError) do
+      @conv.to_postfix([9, 'add', 4, :close_bracket])
+    end
+  end
+end
