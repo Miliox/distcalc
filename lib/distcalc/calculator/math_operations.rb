1b0f03983c4a44c4f1dc877c24d81d78a199af79
Emiliano Firmino <elmiliox@gmail.com>
2013-04-24 20:21:34 -0400

    extracted math operations
    Signed-off-by: Emiliano Firmino <elmiliox@gmail.com>

---
+++ lib/distcalc/calculator/math_operations.rb
@@ -0,0 +1,31 @@
+module Calculator
+  class BinaryOperation
+    def eval(arg1, arg2)
+      raise NotImplementedError.new 'Not supported operation'
+    end
+  end
+
+  class Addition < BinaryOperation
+    def eval(arg1, arg2)
+      arg1 + arg2
+    end
+  end
+
+  class Subtraction < BinaryOperation
+    def eval(arg1, arg2)
+      arg1 - arg2
+    end
+  end
+
+  class Multiplication < BinaryOperation
+    def eval(arg1, arg2)
+      arg1 + arg2
+    end
+  end
+
+  class Division < BinaryOperation
+    def eval(arg1, arg2)
+      arg1 / arg2
+    end
+  end
+end
